c53948546d2faafbedd0254e52904b776edc2ebb
Aryan Verma <aryanverma@Aryans-MacBook-Air.local>
2022-11-17 11:41:13 +0530

    change UI with better form fields

--- app_client/src/App.js
+++ app_client/src/App.js
@@ -1,35 +1,107 @@
 import react, {useState} from 'react'
 import axios from 'axios'
-import ToggleEmail from './components/ToggleEmail'
+import {Button} from '@mui/material';
+import { Input } from '@mui/material';
 
 
 
 const App = () => {
 
   const [content,setContent] = useState('')
-  const [toggleEmail, setToggleEmail] = useState(false)
+  const [toggleState, setToggleState] = useState(false)
+  const [emailID,setEmailID] = useState('')
 
 
 
+    
+
+  const sendEmail = (event) => {
+      console.log(`${content} and ${emailID}`)
+      axios.post('/sendEmail', {
+          emailID,
+          content,
+      }).then(response => {
+          console.log(`Rnesponse has been sent!!!`)
+      })
+
+      setEmailID('')
+      setContent('')
+
+  }
+
+    
+    
+
+
+  
 
   return (
-    <div>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "light-grey"
+    }}>
       <h2>Recaller</h2>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form 
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onSubmit={(e) => e.preventDefault()}>
         <label>Write your thoughts out</label>
         <div>
-          <input placeholder='What do you want to recall later' value={content} onChange={(e) => setContent(e.target.value)}/>
+          <Input
+            style={{
+              width: "60vw",
+              marginTop: "10px",
+              marginBottom: "10px",
+            }} 
+            variant = 'standard' multiline = 'true' placeholder='What do you want to recall later' value={content} onChange={(e) => setContent(e.target.value)}/>
         </div>
         <div>
         {
-          toggleEmail === false
-          ?<button onClick={() => {setToggleEmail(true)} }>Send to email</button>
+          toggleState === false
+          ?<Button size = 'medium' variant = 'outlined' onClick={() => {setToggleState(true)} }>Send to email</Button>
           :null
         }
         </div>
-        <div>
-          <ToggleEmail toggleValue={toggleEmail} content= {content}/>
-        </div>
+        {
+          toggleState == true
+          ?
+            (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <p>Enter email</p>
+                <Input 
+                  style={{
+                    width: "40vw",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                  }} 
+                  inputProps={{min: 0, style: { textAlign: 'center' }}}
+                  variant='outlined' 
+                  color='secondary'
+                  size='small'
+                  placeholder="Email to send to" value={emailID} onChange={(event) => setEmailID(event.target.value)}/>
+                <Button size = 'medium' variant = 'outlined' onClick={sendEmail}>Send</Button>
+              </div>
+            )
+          : 
+            (
+              null
+            )
+        }
+
       </form>
     </div>
   )
