ccbb78d7c489d6b34c4bb5b5936811a5a220a01e
Donald Iloekwe <donytex4u@gmail.com>
2022-11-29 18:22:54 +0000

    set schedule selection button text color to a lighter tone

--- app_client/src/App.js
+++ app_client/src/App.js
@@ -1,43 +1,38 @@
-import react, {useState} from 'react'
-import axios from 'axios'
-import {Button} from '@mui/material';
+import react, { useState } from 'react';
+import axios from 'axios';
+import { Button } from '@mui/material';
 import { Input } from '@mui/material';
-import { TextField } from '@mui/material'
+import { TextField } from '@mui/material';
 
 import logo from './recaller-logo-light.png';
 
 // ...later
 
-import Select from 'react-select'
+import Select from 'react-select';
 
 const App = () => {
-
   //config for dropdown
   const customStyles = {
     control: (base, state) => ({
       ...base,
-      background: "transparent",
+      background: 'transparent',
       // match with the menu
-      borderRadius: state.isFocused ? "3px 3px 0 0" : 3,
+      borderRadius: state.isFocused ? '3px 3px 0 0' : 3,
       // Overwrittes the different states of border
-      borderColor: state.isFocused ? "lightGrey" : "grey",
+      borderColor: state.isFocused ? 'lightGrey' : 'grey',
       // Removes weird border around container
       boxShadow: state.isFocused ? null : null,
-      "&:hover": {
+      '&:hover': {
         // Overwrittes the different states of border
-        borderColor: state.isFocused ? "lightGrey" : "lightGrey"
-      }
+        borderColor: state.isFocused ? 'lightGrey' : 'lightGrey',
+      },
     }),
-    option: (styles, {isFocused, isSelected}) => ({
-      ...styles, 
-      background: isFocused
-          ? 'grey'
-          : isSelected
-              ? 'dark'
-              : undefined,
-      zIndex: 1
-  }),
-    menu: base => ({
+    option: (styles, { isFocused, isSelected }) => ({
+      ...styles,
+      background: isFocused ? 'grey' : isSelected ? 'dark' : undefined,
+      zIndex: 1,
+    }),
+    menu: (base) => ({
       ...base,
       // override border radius to match the box
       backgroundColor: 'black',
@@ -46,44 +41,43 @@ const App = () => {
       marginTop: 0,
       zIndex: 100,
     }),
-    menuList: base => ({
+    menuList: (base) => ({
       ...base,
       // kill the white space on first and last option
-      padding: 0
-    })
+      padding: 0,
+    }),
+    // set schedule selection button text color to grey
+    singleValue: (base) => ({
+      ...base,
+      color: 'grey',
+    }),
   };
-  
 
+  const [content, setContent] = useState('');
+  const [toggleState, setToggleState] = useState(false);
+  const [emailID, setEmailID] = useState('');
+  const [days, setDays] = useState({ value: '0', label: 'right now' });
 
-
-  const [content,setContent] = useState('')
-  const [toggleState, setToggleState] = useState(false)
-  const [emailID,setEmailID] = useState('')
-  const [days, setDays] = useState({value: '0', label: 'right now'})
-  
   const handleDayChange = (obj) => {
-    setDays(obj)
-  }
+    setDays(obj);
+  };
 
   const sendEmail = (event) => {
+    const sendObject = {
+      emailID: emailID,
+      content: content,
+      days: days.value,
+    };
 
-      const sendObject = {
-        emailID: emailID,
-        content: content,
-        days: days.value,
-      }
+    console.log('Obejct to be sent is', sendObject);
+    axios.post('/sendEmail', sendObject).then((response) => {
+      console.log(`Response has been sent!!!`);
+    });
 
-      console.log('Obejct to be sent is', sendObject)
-      axios.post('/sendEmail', sendObject).then(response => {
-          console.log(`Response has been sent!!!`)
-
-      })
-
-      setEmailID('')
-      setContent('')
-      setDays(days.value = '0', days.label = 'right now')
-
-  }
+    setEmailID('');
+    setContent('');
+    setDays((days.value = '0'), (days.label = 'right now'));
+  };
   //dayselect logic
 
   const options = [
@@ -92,102 +86,113 @@ const App = () => {
     { value: '0.25', label: '6 hours later' },
     { value: '1', label: 'tomorrow' },
     { value: '7', label: 'next week' },
-  ]
-
+  ];
 
   return (
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "linear-gradient(#e66465, #9198e5)"
-      }}>
-        <h1>
-          <a href="#">
-          <img src={logo} alt="recaller logo" height={'50vw'} />
-          </a>
-        </h1>
-        <form 
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            color: '#f5f4f2',
-          }}
-          onSubmit={(e) => e.preventDefault()}>
-          <h3>what do you want to recall later?</h3>
-          <div>
-            <Input
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'linear-gradient(#e66465, #9198e5)',
+      }}
+    >
+      <h1>
+        <a href='#'>
+          <img src={logo} alt='recaller logo' height={'50vw'} />
+        </a>
+      </h1>
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#f5f4f2',
+        }}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <h3>what do you want to recall later?</h3>
+        <div>
+          <Input
+            sx={{
+              width: '80vw',
+              marginTop: '20px',
+              marginBottom: '20px',
+              background: 'transparent',
+              outline: 'none',
+              overflow: 'auto',
+              fontSize: '1em',
+              color: 'white',
+            }}
+            multiline={true}
+            placeholder='start typing'
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <div>
+          {toggleState === false ? (
+            <div>
+              <Button
+                style={{
+                  marginRight: '20px',
+                }}
+                size='medium'
+                variant='outlined'
+                onClick={() => {
+                  setToggleState(true);
+                }}
+              >
+                Send to Email
+              </Button>
+              <Button
+                style={{
+                  color: 'grey',
+                  borderColor: 'grey',
+                }}
+                disabled={true}
+                size='medium'
+                variant='outlined'
+                onClick={() => {
+                  setToggleState(false);
+                }}
+              >
+                Send to Whatsapp
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        {toggleState === true ? (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
               sx={{
-                width: "80vw",
-                marginTop: "20px",
-                marginBottom: "20px",
-                background: "transparent",
-                outline: "none",
-                overflow: "auto",
-                fontSize: "1em",
-                color: "white"
-
-              }} 
-              
-              multiline= {true}
-              placeholder='start typing' value={content} onChange={(e) => setContent(e.target.value)}
-
+                width: '300px',
+                '& .MuiInputLabel-root': { color: 'lightGrey' }, //styles the label
+                '& .MuiOutlinedInput-root': {
+                  '& > fieldset': { borderColor: 'default' },
+                },
+              }}
+              inputProps={{
+                style: {
+                  color: 'white',
+                },
+              }}
+              id='outlined-basic'
+              label='Email'
+              variant='outlined'
+              value={emailID}
+              onChange={(event) => setEmailID(event.target.value)}
             />
-            
-          </div>
-          <div>
-          {
-            toggleState === false
-            ?<div>
-                <Button 
-                  style = {{
-                    marginRight: '20px'
-                  }}
-                  size = 'medium' variant = 'outlined' onClick={() => {setToggleState(true)} }>Send to Email
-                </Button>
-                <Button 
-                  style = {{
-                     color: "grey",
-                     borderColor: "grey",
-                  }}
-                  disabled = {true} size = 'medium' variant = 'outlined' onClick={() => {setToggleState(false)} }>Send to Whatsapp
-                </Button>
-              </div>
-            :null
-          }
-          </div>
-          {
-            toggleState === true
-            ?
-              (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <TextField 
-                    sx={{
-                      width: "300px",
-                      "& .MuiInputLabel-root": {color: 'lightGrey'},//styles the label
-                      "& .MuiOutlinedInput-root": {
-                        "& > fieldset": { borderColor: "default" },
-                      },
-                    }}
-                  
-                    inputProps={{ style: 
-                      { 
-                        color: "white",
-                        
-                      } 
-                    }}
-                    id="outlined-basic" label="Email" variant="outlined" value={emailID} onChange={(event) => setEmailID(event.target.value)} />
-                  {/* <Input 
+            {/* <Input 
                     style={{
                       width: "40vw",
                       marginTop: "10px",
@@ -198,36 +203,33 @@ const App = () => {
                     color='secondary'
                     size='small'
                     placeholder="Email to send to" value={emailID} onChange={(event) => setEmailID(event.target.value)}/> */}
-                  <p>when to schedule?</p>
-            
-                  <Select
-                    styles = {customStyles}
-                    value={days}
-                    options={options}
-                    onChange={handleDayChange}
-                    getOptionLabel={x => x.label}
-                    getOptionValue={x => x.value} />
-                    
-                  <Button 
-                    style={{
-                      marginTop: "10px",
-                      marginBottom: "10px",
-                    }} 
-                    size = 'medium' variant = 'outlined' onClick={sendEmail}>Recall</Button>
-                  
-                </div>
-              )
-            : 
-              (
-                null
-              )
-          }
-        
-          
+            <p>when to schedule?</p>
 
-        </form>
-      </div>
-  )
-}
+            <Select
+              styles={customStyles}
+              value={days}
+              options={options}
+              onChange={handleDayChange}
+              getOptionLabel={(x) => x.label}
+              getOptionValue={(x) => x.value}
+            />
 
-export default App
+            <Button
+              style={{
+                marginTop: '10px',
+                marginBottom: '10px',
+              }}
+              size='medium'
+              variant='outlined'
+              onClick={sendEmail}
+            >
+              Recall
+            </Button>
+          </div>
+        ) : null}
+      </form>
+    </div>
+  );
+};
+
+export default App;
